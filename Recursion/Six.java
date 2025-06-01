a3ced717ba8cc9751d1ec837cca813c133cd71f6
VairagPatel <vairag.techwork@gmail.com>
2025-06-01 22:55:19 +0530

    Check if array is sorted or not

--- Recursion/Six.java
+++ Recursion/Six.java
@@ -1,5 +1,13 @@
 public class Six {
     public static boolean Issorted(int[] arr,int i){
+
+        // for(int i = 0;i<arr.length;1++){
+        //     if(arr[i]>arr[i+1]){
+        //         return false;
+        //     }
+        //     return true;
+        // }
+
         if(i == arr.length-1){
             return true;
         }
@@ -9,7 +17,7 @@ public class Six {
         return Issorted(arr,i+1);   
     }
     public static void main(String[] args) {
-        int[] arr = {1, 2, 3, 4, 5};
+        int[] arr = {1, 2, 7, 4, 5};
         boolean result = Issorted(arr, 0);
         if (result) {
             System.out.println("The array is sorted.");
