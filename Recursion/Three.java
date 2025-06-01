236fb3988a5f4a3773ef6374a3445e3e77aabeea
VairagPatel <vairag.techwork@gmail.com>
2025-06-01 21:10:46 +0530

    Factorial

--- Recursion/Three.java
+++ Recursion/Three.java
@@ -12,3 +12,5 @@ public class Three {
         System.out.println("Factorial of " + n + " is: " + result);
     }
 }
+// This code defines a recursive method to calculate the factorial of a number.
+// The base case is when n is 0 or 1, returning 1. Otherwise, it multiplies n by the factorial of (n - 1).          
