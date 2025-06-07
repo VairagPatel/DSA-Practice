2f4ffc493e53c6bab2440f7cacccc40f125d03ad
VairagPatel <vairag.techwork@gmail.com>
2025-06-07 09:39:31 +0530

    Linkdlist

---
+++ LinkedList/Linkedlist.java
@@ -0,0 +1,63 @@
+import java.util.LinkedList;
+
+public class Linkedlist{
+    public static class Node{
+        int data;
+        Node next;
+
+        public Node(int data){
+            this.data = data;
+            this.next=null;
+        }
+    }
+    public static Node head;
+    public static Node tail;
+
+    public void addFirst(int data){
+        //Step-1 Create new node
+        Node newNode = new Node(data);
+        if(head==null){
+            head=tail=newNode;
+            return;
+        }
+        //step-2 newNode next = head
+        newNode.next = head;
+        //step-3 head = newNode
+        head = newNode;
+    }
+
+    public void addLast(int data){
+        Node newNode = new Node(data);
+        if(head==null){
+            head=tail=newNode;
+            return;
+        }
+        tail.next = newNode;
+        tail = newNode;
+    }
+
+    public void print(){
+        if(head==null){
+            System.out.println("LL is empty");
+            return;
+        }
+        Node temp = head;
+        while(temp!=null){
+            System.out.print(temp.data+"->");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+    public static void main(String[] args) {
+        Linkedlist ll = new Linkedlist();
+        ll.print();
+        ll.addFirst(1);
+        ll.print();
+        ll.addFirst(2);
+        ll.print();
+        ll.addLast(3);
+        ll.print();
+        ll.addLast(4);
+        ll.print();
+    }
+}
