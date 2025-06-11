796cebe77f6267fbbab011cae8f68e1188d2a05d
VairagPatel <vairag.techwork@gmail.com>
2025-06-12 04:01:08 +0530

    Valid Parenthesis using Stack

--- Stacks/Stack6.java
+++ Stacks/Stack6.java
@@ -3,10 +3,8 @@ public class Stack6 {
 
     public static boolean isValid(String str){
         Stack<Character> s = new Stack<>();
-
         for (int i=0;i<str.length();i++){
             char ch = str.charAt(i);
-
             if ( ch =='(' || ch=='{' || ch == '['){
                 s.push(ch);
             }else{
