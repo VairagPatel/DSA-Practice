c4685e0c637df46750624f3baa327b456bbfa9ec
VairagPatel <vairag.techwork@gmail.com>
2025-06-15 01:42:48 +0530

    fractional knapsack

---
+++ Greedy algorith/Class2.java
@@ -0,0 +1,10 @@
+
+public class Class2 {
+    public static void main(String[] args) {
+        int val[]= {60,100,120};
+        int wieght[] = {10,20,30};
+        int w = 50;
+
+        
+    }    
+}
